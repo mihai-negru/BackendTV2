f8944bea209520746f24ee1637e3bf594b52f13c
Mihai Negru <determinant289@gmail.com>
2023-01-03 12:18:20 +0200

    Finished setting up the project

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,48 @@
+import backendtv.server.ServerApp;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import datafetch.DataFetch;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * <p>Entry point for the backendTV application.</p>
+ *
+ * @see ObjectMapper
+ * @see ArrayNode
+ * @since 1.0.0
+ * @author Mihai Negru
+ */
+public final class Main {
+    private Main() {
+        // Do not let anyone instantiate this class
+    }
+
+    /**
+     * <p>Takes two {@code String} objects as two file paths.</p>
+     *
+     * <p>Opens first file for readying and fetches the data into the
+     * {@code DataFetch} class which will be processed in the main
+     * application.</p>
+     *
+     * <p>Opens second file for writing and prints all the collected
+     * information into file as a JSON object.</p>
+     * @param args files that need to be processed.
+     */
+    public static void main(final String[] args) throws IOException, IllegalArgumentException {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Bad main args list");
+        }
+
+        final ObjectMapper objectMapper = new ObjectMapper();
+
+        DataFetch inputData = objectMapper.readValue(new File(args[0]), DataFetch.class);
+
+        final ArrayNode outputData = objectMapper.createArrayNode();
+
+        ServerApp.connect().start(inputData, outputData);
+
+        objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File(args[1]), outputData);
     }
 }
