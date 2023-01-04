43ba86c7930d9ab94fbd45d21a3db1b7c76030ed
Mihai Negru <determinant289@gmail.com>
2023-01-04 19:12:02 +0200

    Modified the back action in order to show output messages (not only error messages, but also success messages)

--- src/backendtv/process/actiontype/pagesactions/ChangePageBackAction.java
+++ src/backendtv/process/actiontype/pagesactions/ChangePageBackAction.java
@@ -1,5 +1,6 @@
 package backendtv.process.actiontype.pagesactions;
 
+import backendtv.pagestype.PageType;
 import backendtv.parser.JsonParser;
 import backendtv.process.actiontype.ActionCommand;
 import backendtv.server.ServerApp;
@@ -20,6 +21,22 @@ public final class ChangePageBackAction implements ActionCommand {
             if (!client.changePageBack()) {
                 JsonParser.parseBasicError(parserObject);
                 output.add(parserObject);
+            } else {
+                final var clientPage = client.getLoadedPage();
+
+                if (clientPage == PageType.MOVIES) {
+                    parserObject.putNull("error");
+                    JsonParser.parseAvailableMovies(parserObject.putArray("currentMoviesList"));
+                    JsonParser.parseClient(parserObject);
+
+                    output.add(parserObject);
+                } else if (clientPage == PageType.DETAILS) {
+                    parserObject.putNull("error");
+                    JsonParser.parseMovie(parserObject.putArray("currentMoviesList"), "name", client.getSeeMovie());
+                    JsonParser.parseClient(parserObject);
+
+                    output.add(parserObject);
+                }
             }
         } else {
             JsonParser.parseBasicError(parserObject);
